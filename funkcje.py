a2dbd9ff72e5d6b5f0c4d3b8c23ae879756f518d
Paweł Jaszczerski <74678964+PawelJaszczerski@users.noreply.github.com>
2020-11-26 23:23:34 +0100

    Global omega, T

--- funkcje.py
+++ funkcje.py
@@ -6,6 +6,12 @@ import math
 # omega - częstość kołowa [rad/s]
 # T - okres [s]
 g = 9.8067
-def okres(d):
-    T= 2*math.pi*(d/g)**(1/2)
+def częstość(d):
+    global omega
+    omega = d/g
+    return omega
+def okres(omega):
+    global T
+    T= 2*math.pi*(omega)**(1/2)
     return T
+
