3d04578bd1717acee7d75e792cbe4a7ddeaa6b60
jania <a.com>
2020-11-26 23:00:06 +0100

    def okres + poprawki

--- funkcje.py
+++ funkcje.py
@@ -1,7 +1,11 @@
-# d - długość wahadła
-# theta - kąt wychylenia początkowego
+import math
+# d - długość wahadła [m]
+# theta_0 - kąt wychylenia początkowego [rad]
 # g - przyspieszenie grawitacyjne (docelowo ziemskie jako domyślne lub wprowadzane przez użytkownika)
-g = 9,8067
-# m - masa punktowa
-# omega - częstość kołowa
-# T - okres
+# m - masa punktowa [kg]
+# omega - częstość kołowa [rad/s]
+# T - okres [s]
+g = 9.8067
+def okres(d):
+    T= 2*math.pi*(d/g)**(1/2)
+    return T
