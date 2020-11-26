5d429fa3a4e26e132b487ebffbe9537efc26939c
Paweł Jaszczerski <74678964+PawelJaszczerski@users.noreply.github.com>
2020-11-26 22:42:15 +0100

    Zdefiniowanie danych

--- funkcje.py
+++ funkcje.py
@@ -1,2 +1,7 @@
 # d - długość wahadła
 # theta - kąt wychylenia początkowego
+# g - przyspieszenie grawitacyjne (docelowo ziemskie jako domyślne lub wprowadzane przez użytkownika)
+g = 9,8067
+# m - masa punktowa
+# omega - częstość kołowa
+# T - okres
