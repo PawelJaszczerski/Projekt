2bed22f26bf22a85d6b235b85628eb53ba01cb5f
Paweł Jaszczerski <74678964+PawelJaszczerski@users.noreply.github.com>
2020-12-04 13:35:33 +0100

    plot

--- Wykresy.py
+++ Wykresy.py
@@ -2,7 +2,14 @@ import funkcje as fn
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-time = np.linspace(0., 1000., 10000)
+time = np.linspace(0., 100., 1000)
+plt.subplot(222)
 angle = np.vectorize(fn.angle)
-plt.plot(time, angle(time, math.pi*1/12, 0.5))
+plt.plot(time, angle(time, math.pi*1/12, 1), label='angle')
+plt.subplot(212)
+pot_en = np.vectorize(fn.pot_en)
+plt.plot(time, pot_en(time, 1, math.pi*1/12, 1), color='r', label='pot_en')
+plt.subplot(222)
+kin_en = np.vectorize(fn.kin_en)
+plt.plot(time, kin_en(time, 1, math.pi*1/12, 1), color='g', label='kin_en')
 plt.show()
