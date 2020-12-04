70ac994a8bd393438706fde4236c5a78a6827767
Paweł Jaszczerski <74678964+PawelJaszczerski@users.noreply.github.com>
2020-12-04 13:00:45 +0100

    angle

--- Wykresy.py
+++ Wykresy.py
@@ -1,7 +1,8 @@
-import numpy as np
 import funkcje as fn
 import matplotlib.pyplot as plt
-t=np.linspace(0,10,100)
-print(t)
-fn.angle(1,1,1)
-#plt.plot(t,)
+import numpy as np
+import math
+time = np.linspace(0., 1000., 10000)
+angle = np.vectorize(fn.angle)
+plt.plot(time, angle(time, math.pi*1/12, 0.5))
+plt.show()
