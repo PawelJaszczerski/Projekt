548181cc1878b2c6680caa17d367182b2828367c
jania <a.com>
2020-12-04 11:26:42 +0100

    tłumaczenie

--- funkcje.py
+++ funkcje.py
@@ -7,33 +7,33 @@ import math
 # T - okres [s]
 # t - czas [s]
 g = 9.8067
-def czestosc(d):
+def angfreq(d):
     global omega
     omega = d/g
     return omega
-def okres(d):
+def period(d):
     global T
-    czestosc(d)
+    angfreq(d)
     T = 2*math.pi*(omega)**(1/2)
     return T
-def wychylenie(t, theta_0, d):
-    czestosc(d)
+def angle(t, theta_0, d):
+    angfreq(d)
     global theta
     theta=theta_0* math.sin(omega*t+theta_0)
     return theta
-def energia_potencjalna(t, d, theta_0, m):
-    wychylenie(t, theta_0, d)
+def pot_en(t, d, theta_0, m):
+    angle(t, theta_0, d)
     global Ep
     Ep = m*g*d*(1-math.cos(theta))
     return Ep
-def energia_kinetyczna(t, d, theta_0, m):
-    wychylenie(t, theta_0, d)
+def kin_en(t, d, theta_0, m):
+    angle(t, theta_0, d)
     global Ek
     Ek = m*g*d*(math.cos(theta)-math.cos(theta_0))
     return Ek
-def energia_calkowita(t, d, theta_0, m):
-    energia_potencjalna(t, d, theta_0, m)
-    energia_kinetyczna(t, d, theta_0, m)
+def tot_en(t, d, theta_0, m):
+    pot_en(t, d, theta_0, m)
+    kin_en(t, d, theta_0, m)
     global Ec
     Ec = Ek + Ep
     return Ec
