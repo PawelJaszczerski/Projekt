6ee684697166f600298fde1e748f03be87965f12
Paweł Jaszczerski <74678964+PawelJaszczerski@users.noreply.github.com>
2020-12-15 21:24:37 +0100

    Animacja (ze "sznurkiem")

--- animacja.py
+++ animacja.py
@@ -8,20 +8,28 @@ g = 9.8067
 d = 1
 m = 1
 t = np.arange(0., 1000., 0.1)
-theta_0 = 1/24*np.pi
+theta_0 = 1/36*np.pi
 
 fig = plt.figure()
 
-plt.xlim(-d*2, d*2)
-plt.ylim(0, d*2)
+plt.xlim(-d, d)
+plt.ylim(-d/5, d)
 
-lines = plt.plot([], 'o')
+points = plt.plot([], 'bo')
+point = points[0]
+
+xline0, yline0 = 0, d
+lines = plt.plot([], [], '-')
 line = lines[0]
 
 def animation(frame):
+    global x, y
     x = d *  np.sin(fn.angle(frame, theta_0, d))
     y = d - d *  np.cos(fn.angle(frame, theta_0, d))
-    line.set_data((x, y))
+    point.set_data(x, y)
+    xline = x
+    yline = y
+    line.set_data([xline0, xline], [yline0, yline])
 
 ani = ani.FuncAnimation(fig, animation, frames=10000, interval=10)
 plt.show()
