63f1ca632fd3e0c402b5a91b8412f763786f9ed7
Paweł Jaszczerski <74678964+PawelJaszczerski@users.noreply.github.com>
2020-12-16 20:09:15 +0100

    Wykresy velocity

--- wykresy.py
+++ wykresy.py
@@ -5,21 +5,27 @@ import math
 
 time = np.linspace(0., 50., 1000)
 
-plt.subplot(311)
+plt.subplot(411)
 angle = np.vectorize(fn.angle)
 plt.plot(time, angle(time, np.pi*1/36, 1), label='angle')
 plt.xlabel('time')
 plt.ylabel('angle')
 
-plt.subplot(312)
+plt.subplot(412)
 pot_en = np.vectorize(fn.pot_en)
 plt.plot(time, pot_en(time, 1, np.pi*1/36, 1), color='r', label='pot_en')
 plt.xlabel('time')
 plt.ylabel('pot_en')
 
-plt.subplot(313)
+plt.subplot(413)
 kin_en = np.vectorize(fn.kin_en)
 plt.plot(time, kin_en(time, 1, np.pi*1/36, 1), color='g', label='kin_en')
 plt.xlabel('time')
 plt.ylabel('kin_en')
+
+plt.subplot(414)
+velocity = np.vectorize(fn.velocity)
+plt.plot(time, velocity(time, 1, np.pi*1/36, 1), color='y', label='velocity')
+plt.xlabel('time')
+plt.ylabel('velocity')
 plt.show()
