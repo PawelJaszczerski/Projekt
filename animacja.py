330d821df96ace0207672781ed732c57ae87574d
Paweł Jaszczerski <74678964+PawelJaszczerski@users.noreply.github.com>
2021-01-02 21:16:52 +0100

    Funkcja

--- animacja.py
+++ animacja.py
@@ -1,37 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.animation as ani
+from matplotlib.animation import FuncAnimation
 import funkcje as fn
 import in_out as io
 
-io.param()
-#przykładowe dane
-g = 9.8067
-d=io.d
-m=io.m
-t = np.arange(0., 1000., 0.1)
-theta_0=np.radians(io.theta_0)
+def animacja():
+    io.param()
+    #przykładowe dane
+    g = 9.8067
+    d=io.d
+    m=io.m
+    t = np.arange(0., 1000., 0.1)
+    theta_0=np.radians(io.theta_0)
 
-fig = plt.figure()
+    fig = plt.figure()
 
-plt.xlim(-d, d)
-plt.ylim(-d/5, d)
+    plt.xlim(-d, d)
+    plt.ylim(-d/5, d)
 
-points = plt.plot([], 'bo')
-point = points[0]
+    points = plt.plot([], 'bo')
+    point = points[0]
 
-xline0, yline0 = 0, d
-lines = plt.plot([], [], '-')
-line = lines[0]
+    xline0, yline0 = 0, d
+    lines = plt.plot([], [], '-')
+    line = lines[0]
 
-def animation(frame):
-    global x, y
-    x = d * np.sin(fn.angle(frame, theta_0, d))
-    y = d - d * np.cos(fn.angle(frame, theta_0, d))
-    point.set_data(x, y)
-    xline = x
-    yline = y
-    line.set_data([xline0, xline], [yline0, yline])
+    def animation(frame):
+        global x, y
+        x = d * np.sin(fn.angle(frame, theta_0, d))
+        y = d - d * np.cos(fn.angle(frame, theta_0, d))
+        point.set_data(x, y)
+        xline = x
+        yline = y
+        line.set_data([xline0, xline], [yline0, yline])
 
-ani = ani.FuncAnimation(fig, animation, frames=10000, interval=10)
-plt.show()
+    ani = FuncAnimation(fig, animation, frames=10000, interval=10)
+    plt.show()
+
