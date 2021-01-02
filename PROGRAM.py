38e4ed4e7a6537eb4ba3eb031f9424de0f2c62ad
Paweł Jaszczerski <74678964+PawelJaszczerski@users.noreply.github.com>
2021-01-02 22:36:37 +0100

    Poprawne

--- PROGRAM.py
+++ PROGRAM.py
@@ -3,7 +3,7 @@ import in_out as io
 try:
     io.param()
 except ValueError:
-    print('Proszę wprowadzić dane do pliku i uruchomić program ponownie')
+    print('Proszę wprowadzić poprawne dane do pliku i uruchomić program ponownie')
 else:
     try:
         io.output()
