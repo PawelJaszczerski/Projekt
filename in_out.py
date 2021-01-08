0b6103dfdd92e276fdbdef1f30cefed32b7bec9b
jania <a.com>
2021-01-08 22:51:57 +0100

    proba cmt

--- in_out.py
+++ in_out.py
@@ -1,37 +1,53 @@
+import funkcje as fn
+import os
 import numpy as np
+def param():
+    try:
+        data=open('data.txt', 'r')
+        dist=data.readline()
+        init_ang=data.readline()
+        mass=data.readline()
+        acceleration=data.readline()
+        global d
+        global theta_0
+        global m
+        global g
+        d = float(dist.split(':')[1])
+        theta_0 = float(init_ang.split(':')[1])
+        m = float(mass.split(':')[1])
+        try:
+            g = float(acceleration.split(':')[1])
+        except ValueError:
+            g = 9.8067
+        return d, theta_0, m, g
 
-def angfreq(d, g):
-    global omega
-    omega = (g/d)**(1/2)
-    return omega
-def period(d, g):
-    global T
-    angfreq(d, g)
-    T = 2*np.pi*(1/omega)
-    return T
-def angle(t, theta_0, d, g):
-    angfreq(d, g)
-    global theta
-    theta=theta_0* np.cos(omega*t)
-    return theta
-def pot_en(t, d, theta_0, m, g):
-    angle(t, theta_0, d, g)
-    global Ep
-    Ep = m*g*d*(1-np.cos(theta))
-    return Ep
-def kin_en(t, d, theta_0, m, g):
-    angle(t, theta_0, d, g)
-    global Ek
-    Ek = m*g*d*(np.cos(theta)-np.cos(theta_0))
-    return Ek
-def tot_en(d, theta_0, m, g):
-    global Ec
-    Ec = m*g*d*(1-np.cos(theta_0))
-    return Ec
-def velocity(t, d, theta_0, m, g):
-    global v
-    kin_en(t, d, theta_0, m, g)
-    v = (2*Ek/m)**(1/2)
-    return v
+    except FileNotFoundError:
+        data=open('data.txt', 'a')
+        data.write('d[m]:')
+        data.write('\ntheta_0[stopnie]:')
+        data.write('\nm[kg]:')
+        data.write('\n(opcjonalnie)g[m/s^2]:')
+        data.close()
 
 
+
+def output():
+    odata=open('data.txt','a')
+    T=round(fn.period(d, g), 4)
+    odata.write('\nT[s]:'+str(T))
+    omega=round(fn.angfreq(d, g), 4)
+    odata.write('\nomega[rad/s]:'+str(omega))
+    Ec=round(fn.tot_en(d,np.radians(theta_0), m, g), 4)
+    odata.write('\nEc[J]:'+str(Ec))
+    odata.close()
+
+    try:
+        os.rename('data.txt','odata.txt')
+    except FileExistsError:
+        os.remove('odata.txt')
+        os.rename('data.txt','odata.txt')
+
+def preview():
+    pdata=open('odata.txt','r')
+    prev=pdata.read()
+    print(prev)
